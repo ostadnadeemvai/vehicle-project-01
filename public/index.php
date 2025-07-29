107f1cb10d80c8599158a31361e7c17d0fab942f
Md Nadim Hossain <mdnadimhossain605602@gmail.com>
2025-07-29 20:34:26 +0600

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "./../app/classes/VehicleManager.php";
+
+$vehicleManager = new VehicleManager("","","","");
+$vehicles = $vehicleManager->getVehicles();
+
+
+include './views/header.php';
+?>
+
+
+<div class="container my-4">
+    <h1>Vehicle Listing</h1>
+    <a href="./../public/views/add.php" class="btn btn-success mb-4">Add Vehicle</a>
+    <div class="row">
+        <!-- Loop Go here -->
+         <?php foreach($vehicles as $id=>$vehicle): ?>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="<?php echo $vehicle['image'] ?>" class="card-img-top" style="height: 200px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $vehicle['name'] ?></h5> 
+                         <p class="card-text">Type:<?php echo $vehicle['type'] ?> </p>
+                        <p class="card-text">Price:<?php echo $vehicle['price'] ?> $</p>
+                        <a href="./views/edit.php?id=<?= $id ?>" class="btn btn-primary">Edit</a>
+                        <a href="./views/delete.php?id=<?= $id ?>" class="btn btn-danger">Delete</a>
+                    </div>
+                </div>
+            </div>
+           <?php endforeach ; ?>
+        <!-- Loop ends here -->
+    </div>
+</div>
+
+</body>
+</html>
